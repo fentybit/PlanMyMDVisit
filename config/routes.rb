34a6c9179c215c7f7b4765332f844323723366e9
fentyhall <fentymhall@gmail.com>
2021-01-04 13:43:17 -0800

    added routes and SignUp basic form_tag view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'application#welcome'
+  get '/signup', to: 'sessions#new'
+  post '/signup', to: 'sessopms#create'
+  get '/login', to: 'users#new?'
+  post '/login', to: 'users#create?'
+  post '/logout', to: 'users#destroy?'
+
   resources :users 
+  # get '/users/new', to: 'users#new', as: 'register' --> register_path
 end
