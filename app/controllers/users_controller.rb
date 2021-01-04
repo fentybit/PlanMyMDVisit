0b87e4115fce63d6dadae373565720246e92c326
fentyhall <fentymhall@gmail.com>
2021-01-04 13:46:34 -0800

    rudimentary actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def index 
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def show 
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+        def user_params 
+            params.require(:user).permit!
+        end
 end
