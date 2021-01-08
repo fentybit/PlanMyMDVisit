e653f74757971a23d0b3c29037d35d95039edea6
fentyhall <fentymhall@gmail.com>
2021-01-07 21:47:08 -0800

    update API scraper

--- app/adapters/api.rb
+++ app/adapters/api.rb
@@ -9,9 +9,28 @@ class API
         JSON.parse(response.body)
     end 
 
-    def self.insurance_data 
-        binding.pry
-        puts betterdoctor
-    end 
+    def self.doctors_information
+        doctors_array = []
+        data.each do |row|
+            doctor = {}
+            if !row.empty?
+                doctor[:firstname] = row["frst_nm"].capitalize
+                doctor[:lastname] = row["lst_nm"].capitalize
+                doctor[:username] = (row["frst_nm"][0] + row["lst_nm"]).downcase
+                doctor[:email] = (row["frst_nm"][0] + row["lst_nm"]).downcase + "@me.com"
+                doctor[:password] = (row["frst_nm"][0] + row["lst_nm"]).downcase
+                doctor[:gender] = row["gndr"]
 
+                doctor[:specialty] = row["pri_spec"].titleize
+                doctor[:hospital] = row["hosp_afl_lbn_1"].titleize
+                doctor[:address] = row["adr_ln_1"].titleize
+                doctor[:city] = row["cty"].titleize
+                doctor[:state] = row["st"]
+                doctor[:zipcode] = row["zip"][0..4]
+
+                doctors_array << doctor 
+            end 
+        end 
+    end 
 end 
+
