4c13a58d778f7fb2d8870b4664d43d04ae1b28ec
fentyhall <fentymhall@gmail.com>
2021-01-11 19:46:02 -0800

    update healthcare team show page

--- app/models/healthcare_team.rb
+++ app/models/healthcare_team.rb
@@ -4,11 +4,4 @@ class HealthcareTeam < ApplicationRecord
 
     # validates :department, presence: true 
     # validates :appointment, numericality: { less_than_or_equal_to: Date.today.year }
-
-    # .strftime("%B %d, %Y at %k:%M")
-    # def date_time_not_in_past
-    #     if self.appointment <= DateTime.now
-    #         errors.add('Appointment time can not in the past')
-    #     end 
-    # end 
 end 
