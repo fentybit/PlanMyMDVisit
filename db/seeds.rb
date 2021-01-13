adc0ba63df30379c67dc85a1135e4c50f4f24b96
fentyhall <fentymhall@gmail.com>
2021-01-12 21:54:19 -0800

    revise seeds.rb to have user as admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,14 @@ HealthcareTeam.destroy_all
 
 
 # Set up first User Patient
-primary_user = User.create(firstname: "Fenty", lastname: "Hall", username: "fenty", email: "fenty@me.com", password: "fenty")
+primary_user = User.create(firstname: "Fenty", lastname: "Hall", username: "fenty", email: "fenty@me.com", password: "fenty", password_confirmation: "fenty")
 primary_user.patients.create(
     medical_record: "Patient has a mild osteoarthritis. For the sake of the patient's wellbeing, it is advised that the patient must refrain from carrying heavy materials and must allocate up to 30 minutes of exercise daily.", 
     test_results: "20210107 - X-Ray results suggest mild osteoarthritis.", 
     medications: "Ibuprofen, 600 mg; thrice a day, after meal. Noproxen Sodium, 500 mg; Once A Day.")
 
+# Set up first User Admin 
+admin = User.create(firstname: "Shelby", lastname: "Hall", username: "admin", email: "shelby@me.com", password: "admin", password_confirmation: "admin")
 
 # Instantiate Doctors from CMS JSON Data
 JSON.parse(File.read("doctors.json")).each do |doctor|
