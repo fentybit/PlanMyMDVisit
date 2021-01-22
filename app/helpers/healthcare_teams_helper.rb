0aecee6a579e0ce4eb8a2ebd5e270f3974ed1a25
fentyhall <fentymhall@gmail.com>
2021-01-21 18:00:19 -0800

    added helper method for patient show page

--- app/helpers/healthcare_teams_helper.rb
+++ app/helpers/healthcare_teams_helper.rb
@@ -1,13 +1,13 @@
 module HealthcareTeamsHelper
-    def appointment_time(healthcare_team)
-        healthcare_team.appointment.strftime("%A, %B %d, %Y at %l:%M %p")
-    end 
-
     def time(appointment)
         appointment.strftime("%l:%M %p")
     end 
-
+    
     def date_time(appointment)
         appointment.strftime("%B %d, %Y at %l:%M %p")
+    end 
+    
+    def appointment_time(healthcare_team)
+        healthcare_team.appointment.strftime("%A, %B %d, %Y at %l:%M %p")
     end 
 end
--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,2 +1,5 @@
 module PatientsHelper
+    def twolatest_careteams(patient)
+        patient.healthcare_teams.order(appointment: :desc).limit(2)
+    end 
 end
