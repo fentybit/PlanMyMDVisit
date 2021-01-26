3403866a77f830b4d1d3280f154c669bd9dfaaac
fentyhall <fentymhall@gmail.com>
2021-01-25 16:28:35 -0800

    update client_id for authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer unless Rails.env.production?
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-    provider :google_oauth2, ENV["GOOGLE_KEY"],ENV["GOOGLE_SECRET"], skip_jwt: true
+    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_SECRET']
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_SECRET"], skip_jwt: true
 end
